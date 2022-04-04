1ae4d2d93dff2fccb8c3f4c3a45f849b4a707af7
James Hughes <Jameshughes91@live.co.uk>
2022-04-04 15:04:41 +0100

    Main Menu Scene Added

---
+++ Team2ProjectFile/Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public Button yourButton;
+    public string NextLevelName;
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName: NextLevelName);
+    }
+}
